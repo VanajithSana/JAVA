ac1fc22d9361811fc0c5622bcd139368fa312373
VanajithSana <94339473+VanajithSana@users.noreply.github.com>
2023-04-07 10:53:43 +0530

    Create cse310_week1_day1_seee .java
    Write a program to take the 2 inputs of integer type from the user. Display the values separated by symbol ‘-’ if the values are positive otherwise display the message “Invalid Input”.
    
    Input Format
    
    Your program should take at least 2 inputs of integer type.
    
    Constraints
    
    Input should be positive integers
    
    Output Format
    
    Output should be the input values separated by ‘-‘ or “Invalid Input” message if any input is negative.
    
    Sample Input 0
    
    10
    2
    Sample Output 0
    
    10-2
    Sample Input 1
    
    20
    -10
    Sample Output 1
    
    Invalid Inpu

---
+++ cse310_week1_day1_seee .java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class ques1 {
+
+                  public static void main(String[] args) {
+                    Scanner z=new Scanner(System.in);
+                   int a=z.nextInt();
+                   int b=z.nextInt();
+                    if((a>=0)&&(b>=0))
+        {
+            System.out.println(a+"-"+b);
+        }
+                                 else
+        {
+            System.out.println("Invalid Input");
+        }
+    }
+}
