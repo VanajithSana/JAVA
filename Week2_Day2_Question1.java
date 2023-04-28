02c4cea50478be8494252e316dbea58f9a15a7ea
VanajithSana <94339473+VanajithSana@users.noreply.github.com>
2023-04-28 10:13:27 +0530

    Create Week2_Day2_Question1.java
    Write a program to take one input of int type from the user. If the value is positive convert it into double and display it, otherwise display the message “Invalid Input”
    
    Input Format
    
    Your program should take one input of int type.
    
    Constraints
    
    Input should be positive integer
    
    Output Format
    
    If the input value is positive number (including 0) display the value in double type otherwise display the message “Invalid Input”.
    
    Sample Input 0
    
    5
    Sample Output 0
    
    5.0
    Sample Input 1
    
    -1
    Sample Output 1
    
    Invalid Input

---
+++ Week2_Day2_Question1.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+import java.util.*;
+
+public class ques1 {
+
+ public static void main(String[] args) {
+
+ 
+
+ Scanner m=new Scanner(System.in);
+
+ int a=m.nextInt();
+
+ if(a>0){
+
+ float b=a;
+
+ System.out.println(b);
+
+ }
+
+ else{
+
+ System.out.println("Invalid Input");
+
+ }
+
+ }
+
+}
