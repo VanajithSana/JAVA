97ed39e48f43b0089a552c46d57826aceccd2b26
VanajithSana <94339473+VanajithSana@users.noreply.github.com>
2023-04-28 10:24:48 +0530

    Create week2_day5_ques1.java
    Liz built up a software code taking number of 3 different color blocks as input. Considering the end users can enter floating point numbers too as at least 1 block of a color is broken. Design the code converting the not-full numbers to full numbers.
    
    Input Format
    
    Number of balls for three different colors.
    
    Constraints
    
    3 inputs separated with space can be floating points or integers.
    
    Output Format
    
    3 integer numbers
    
    Sample Input 0
    
    2.2 2 2
    Sample Output 0
    
    3 2 2

---
+++ week2_day5_ques1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner obj=new Scanner(System.in);
+        double a=obj.nextDouble();
+        double b=obj.nextDouble();
+        double c=obj.nextDouble();
+        System.out.print((int)Math.ceil(a)+" ");
+        System.out.print((int)Math.ceil(b)+" ");
+        System.out.print((int)Math.ceil(c)+" ");
+    }
+}
