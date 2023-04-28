520ce64bfc7caf50d9cdae80823acae47ecb5774
VanajithSana <94339473+VanajithSana@users.noreply.github.com>
2023-04-28 10:28:55 +0530

    Create week3_day1_ques2.java
    Rahul borrowed an amount from his friend and he needs to repay the total amount within a year with additional interest of 10%. Find the total amount and emi that Rahul has to pay
    
    Input Format
    
    200000
    
    Constraints
    
    The borrowed amount should be in lakhs Interest is based on total amount that Rahul has borrowed from his friend Emi value round off to 2 decimal places
    
    Output Format
    
    220000 18333.33
    
    Sample Input 0
    
    200000
    Sample Output 0
    
    220000
    18333.33

---
+++ week3_day1_ques2.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+import java.util.*;
+
+import java.text.*;
+
+import java.math.*;
+
+import java.util.regex.*;
+
+public class ques2 {
+
+    public static void main(String[] args) 
+
+    {
+
+    Scanner obj=new Scanner(System.in);
+
+    int a=obj.nextInt();
+
+    int v,b;
+
+    v=a/10;
+
+    b=v+a;
+
+    System.out.println(b);
+
+    int e=(b*100)/12;
+
+    System.out.println(e/100F);
+
+   
+
+    }
+
+}
