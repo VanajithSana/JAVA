31afc5a249e222f64ce8b0ecaf497eef6f7d8848
VanajithSana <94339473+VanajithSana@users.noreply.github.com>
2023-04-28 10:14:50 +0530

    Create Week2_Day2_Question2.java
    Sonal built up a software code taking ratings on a product. The product is supposed to be rated between 1 to 5. Create a program displaying an error "101" if the rating is not between 1 to 5, else displaying "thanks".
    
    Input Format
    
    Rating as a number
    
    Constraints
    
    Input can be floating point number or an integer.
    
    Output Format
    
    Error or thanks should be displayed.
    
    Sample Input 0
    
    6
    Sample Output 0
    
    Error: 101
    Sample Input 1
    
    0
    Sample Output 1
    
    Error: 101
    Sample Input 2
    
    3
    Sample Output 2
    
    Thanks
    Sample Input 3
    
    4.0
    Sample Output 3
    
    Thanks

---
+++ Week2_Day2_Question2.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) 
+
+    {
+
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner sc=new Scanner(System.in);
+
+        float a=sc.nextFloat();
+
+        
+
+        if(a>1 && a<5)
+
+        {
+
+            System.out.println("Thanks");
+
+        }
+
+        else
+
+        {
+
+           System.out.println("Error: 101");   
+
+         }
+
+    }
+
+}
