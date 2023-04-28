3864aa24a5c55e9325d7f67df2fcafdfbe4b93bb
VanajithSana <94339473+VanajithSana@users.noreply.github.com>
2023-04-28 10:11:40 +0530

    Create week2_ques2_day1.java
    Consider that two friends Neha and Nisha are going for shopping .Neha has picked the N number of Items from the Market and Nisha has picked X number of Items from the Market .Find out total numbers of Items ordered by both .
    
    Input Format
    
    In First input line, you should enter number of Items ordered by Neha. In Second input line, you should enter number of Items ordered by Nisha.
    
    Constraints
    
    Enter only integer (positive) value. 0<=n<=100
    
    Output Format
    
    Total numbers of items ordered by Neha and Nisha during shopping.
    
    Sample Input 0
    
    5
    10
    Sample Output 0
    
    15

---
+++ week2_ques2_day1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class ques2 {
+
+    public static void main(String[] args)
+    {
+        Scanner z=new Scanner(System.in);
+    int a=z.nextInt();
+    int b=z.nextInt();
+        System.out.println(a+b);
+    }
+    
+}
+        
+        
